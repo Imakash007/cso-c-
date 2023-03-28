a79d6faccb9f6541f1f30d00c85e3e8fd294026e
Imakash007 <129135674+Imakash007@users.noreply.github.com>
2023-03-28 19:01:58 +0530

    Add files via upload

---
+++ code1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  {
+ 
+   
+ float r;
+    
+    scanf("%f", &r);
+    float p,a;
+    p= 2*3.14*r;
+    a=3.14*r*r;
+    printf("%f \n", p);
+    printf("%f \n", a);
+
+    return 0;
+}
---
+++ code2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    float a,b;
+    
+    scanf("%f", &a);
+    scanf("%f", &b);
+    float A1,S1,M1,D1;
+    A1=a+b;
+    S1=a-b;
+    M1=a*b;
+    D1=a/b;
+    printf("%f\n", A1);
+     printf("%f\n", S1);
+     printf("%f\n", M1);
+     printf("%f\n", D1);
+     
+return 0;
+
+}
